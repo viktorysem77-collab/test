bb60bb38f5c243ea54e59fc9d6691bd057d48932
viktorysem77-collab <viktorysem77@gmail.com>
2026-06-16 21:03:02 +0300

    new1

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ else:
     percent = increase / previous * 100
     print(f"Абсолютный прирост: +{increase} баллов")
     print(f"Относительный прирост: {percent:.2f}%")
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+previous = 0
+increase = 20
+
+if previous == 0:
+    percent = float('inf')
+    print(f"Абсолютный прирост: +{increase} баллов")
+    print("Относительный прирост: ∞% (невозможно рассчитать, так как исходный рейтинг равен 0)")
+else:
+    percent = increase / previous * 100
+    print(f"Абсолютный прирост: +{increase} баллов")
+    print(f"Относительный прирост: {percent:.2f}%")
